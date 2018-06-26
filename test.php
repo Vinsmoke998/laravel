6a854101764d2541433f85587a8038971183e0e1
vinsmoke998 <thang.hanu.98@gmail.com>
2018-06-26 09:59:37 +0700

    update this thing

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+class cat extends animal 
+{
+    public function  Greet()
+    {
+
+        return "moew!";
+
+    }
+
+    public function Describe()
+
+    {
+
+        return parrent:: Describe() . ", and i am a boss!";
+    }
+
+}
